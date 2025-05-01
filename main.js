7f53104e60a49025ebcad44d6f73ca333e4f5570
カプール リティカ <kapoor-ri@ibjapan.jp>
2025-05-01 12:04:37 +0900

    warmhole
    with small cube boxes placed randomly inside

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-//basic cube geometry with mesh
-
+//warmhole
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import spline from "./spline.js";
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -12,6 +15,7 @@ renderer.outputColorSpace = THREE. SRGBColorSpace;
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.4);
 const camera = new THREE.PerspectiveCamera(
   75, //fov
   w/h, //aspect
@@ -24,46 +28,95 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geometry = new THREE.BoxGeometry;
-// const material = new THREE.MeshBasicMaterial({ color: 0x0077ff });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-// const geometry = new THREE.IcosahedronGeometry(0.9, 5);
-const material = new THREE.MeshStandardMaterial({
-    color: '#fff',
-    flatShading: true
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+//create a line geometry from the spline
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const meterial = new THREE.LineBasicMaterial({ color: 0x00ff00});
+const line = new THREE.Line(geometry, meterial);
+// scene.add(line);
 
-const wireMat = new THREE.MeshBasicMaterial({
-    color: '#51e0e0',
-    wireframe: true
+const tubeGeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeMat = new THREE.MeshStandardMaterial({
+    color: 0x0099ff,
+    // side: THREE.DoubleSide,
+    wireframe: true,
 })
+const tube = new THREE.Mesh(tubeGeo, tubeMat);
+// scene.add(tube); //blue tube lines
 
-
-const wireMesh = new THREE.Mesh(geometry, wireMat);
-wireMesh.scale.setScalar(1.25);
-mesh.add(wireMesh);
+// create edges geometry from the spline
+const edges = new THREE.EdgesGeometry(tubeGeo, 0.2);
+const lineMat = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const tubeLines = new THREE.LineSegments(edges, lineMat);
+scene.add(tubeLines);
 
 const hemiLight = new THREE.HemisphereLight('#51e0e0', '#757ae0');
 scene.add(hemiLight);
 
-// function animate() {
-//   requestAnimationFrame(animate);
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-//   renderer.render(scene, camera);
-// }
+const numBoxes = 55;
+const size = 0.075;
+const boxGeo = new THREE.BoxGeometry(size, size, size);
+for (let i = 0; i < numBoxes; i += 1) {
+  const boxMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+  });
+  const box = new THREE.Mesh(boxGeo, boxMat);
+  const p = (i / numBoxes + Math.random() * 0.1) % 1;
+  const pos = tubeGeo.parameters.path.getPointAt(p);
+  pos.x += Math.random() - 0.4;
+  pos.z += Math.random() - 0.4;
+  box.position.copy(pos);
+  const rote = new THREE.Vector3(
+    Math.random() * Math.PI,
+    Math.random() * Math.PI,
+    Math.random() * Math.PI
+  );
+  box.rotation.set(rote.x, rote.y, rote.z);
+  const edges = new THREE.EdgesGeometry(boxGeo, 0.2);
+//   const color = new THREE.Color().setHSL(0.7 - p, 1, 0.5);
+  const lineMat = new THREE.LineBasicMaterial({ color: 0xfffff });
+  const boxLines = new THREE.LineSegments(edges, lineMat);
+  boxLines.position.copy(pos);
+  boxLines.rotation.set(rote.x, rote.y, rote.z);
+  // scene.add(box);
+  scene.add(boxLines);
+}
 
-// animate();
-function animate(t){
-    // console.log(t);
-    requestAnimationFrame(animate);
-    // mesh.scale.setScalar(Math.cos(t * 0.001) + 9.0);
-    mesh.rotation.y = (t * 0.0001);
-    controls.update();
-    renderer.render(scene, camera);
+// post-processing
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 100);
+bloomPass.threshold = 0.002;
+bloomPass.strength = 3.5;
+bloomPass.radius = 0;
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+
+function updateCamera(t){
+    const time = t * 0.05;
+    const looptime = 5 * 1000;
+    const p = (time % looptime) / looptime ;
+    const pos = tubeGeo.parameters.path.getPointAt(p);
+    const lookAt = tubeGeo.parameters.path.getPointAt((p + 0.03) % 1);
+    camera.position.copy(pos);
+    camera.lookAt(lookAt);
+}
+
+function animate(t = 0) {
+  requestAnimationFrame(animate);
+  updateCamera(t);
+//   renderer.render(scene, camera);
+  composer.render(scene, camera);
+  controls.update();
 }
 
 animate();
+
+function handleWindowResize(){
+    camera.aspect =window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
